e2cd561c759c9a2e4fcc6087c8595b418c880dbb
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-12-12 13:52:49 +0100

    Testutils refactor

---
+++ ripple-server/test/Ripple/Server/TestUtils.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Ripple.Server.TestUtils (rippleServerSpec) where
+
+import Network.HTTP.Client as HTTP
+import Ripple.API
+import Ripple.Client
+import Ripple.Server
+import Servant.API
+import Servant.Client
+import Test.Syd
+import Test.Syd.Persistent.Sqlite
+import Test.Syd.Servant as Servant
+import Test.Syd.Validity
+import Test.Syd.Wai
+
+rippleServerSpec :: TestDef '[HTTP.Manager] ClientEnv -> Spec
+rippleServerSpec =
+  managerSpec . setupAroundWith' (\man () -> rippleClientEnvSetupFunc man)
+
+rippleClientEnvSetupFunc :: HTTP.Manager -> SetupFunc ClientEnv
+rippleClientEnvSetupFunc man = do
+  env <- envSetupFunc
+  let server = makeRippleServer env
+  Servant.clientEnvSetupFunc rippleAPI man server
+
+envSetupFunc :: SetupFunc Env
+envSetupFunc = do
+  envConnectionPool <- connectionPoolSetupFunc (pure ())
+  let envLogFunc _ _ _ _ = pure ()
+  pure Env {..}
